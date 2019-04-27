e8511e5bfd67da954e3f99792f119fd54a88f5b1
prathap <prathap.mohan@kloctechnologies.co.in>
2019-04-27 19:31:50 +0530

    defined the factories for the user to get some random data

---
+++ spec/factories/user_factory.rb
@@ -0,0 +1,9 @@
+#Factory bot is a fixture replacement, 
+#which allows you to populate sample data while testing application. Here’s how to use it:
+#Create a factory file for specific entity that is used in our application, 
+#considering our example, I am creating spec/factories/user_factory.rb file and it looks like:
+FactoryBot.define do
+  factory :user do
+    name { Faker::Name.name }
+  end
+end
